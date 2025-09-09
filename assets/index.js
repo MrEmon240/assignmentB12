3fda3842aa2f0c7ecc4d5e95c71a95eaf2f28a35
MrEmon240 <mrimon1730@gmail.com>
2025-09-09 06:02:33 +0600

    first commit

---
+++ assets/index.js
@@ -0,0 +1,244 @@
+
+
+document.querySelector(".coll").addEventListener("click", function () {
+ alert("National Emergency Number 999");
+   
+  const collCount = document.querySelector(".collCount");
+  const count = parseInt(collCount.innerText);
+    if (count >! 20) {
+        collCount.innerText = count - 20;
+    }
+    else{
+      alert("You don't have enough coins.");
+    }
+});
+
+document.querySelector(".copy").addEventListener("click", function () {
+  const copyCount = document.querySelector(".copyCount");
+  const count = parseInt(copyCount.innerText);
+  copyCount.innerText = count + 1;
+});
+
+document.querySelector(".heart").addEventListener("click", function () {
+const heartCount = document.querySelector(".heartCount");
+     const count = parseInt(heartCount.innerText);
+     heartCount.innerText = count + 1;
+});
+
+
+
+
+
+
+// tow
+document.querySelector(".collTwo").addEventListener("click", function () {
+ alert("Police Helpline Number 999");
+
+ const collCount = document.querySelector(".collCount");
+ const count = parseInt(collCount.innerText);
+ if (count >! 20) {
+   collCount.innerText = count - 20;
+ } else {
+   alert("You don't have enough coins.");
+ }
+});
+
+document.querySelector(".copyTow").addEventListener("click", function () {
+  const copyCount = document.querySelector(".copyCount");
+  const count = parseInt(copyCount.innerText);
+  copyCount.innerText = count + 1;
+});
+
+document.querySelector(".heartTwo").addEventListener("click", function () {
+const heartCount = document.querySelector(".heartCount");
+     const count = parseInt(heartCount.innerText);
+     heartCount.innerText = count + 1;
+});
+
+// three
+
+
+document.querySelector(".collThree").addEventListener("click", function () {
+ alert("Fire Service Number 999");
+
+ const collCount = document.querySelector(".collCount");
+ const count = parseInt(collCount.innerText);
+ if (count >! 20) {
+   collCount.innerText = count - 20;
+ } else {
+   alert("You don't have enough coins.");
+ }
+});
+
+document.querySelector(".copyTree").addEventListener("click", function () {
+  const copyCount = document.querySelector(".copyCount");
+  const count = parseInt(copyCount.innerText);
+  copyCount.innerText = count + 1;
+});
+
+document.querySelector(".heartThree").addEventListener("click", function () {
+const heartCount = document.querySelector(".heartCount");
+     const count = parseInt(heartCount.innerText);
+     heartCount.innerText = count + 1;
+});
+
+// four
+
+document.querySelector(".collFour").addEventListener("click", function () {
+ alert("Ambulance Service 1994-999999");
+
+ const collCount = document.querySelector(".collCount");
+ const count = parseInt(collCount.innerText);
+ if (count > !20) {
+   collCount.innerText = count - 20;
+ } else {
+   alert("You don't have enough coins.");
+ }
+});
+
+document.querySelector(".copyFour").addEventListener("click", function () {
+  const copyCount = document.querySelector(".copyCount");
+  const count = parseInt(copyCount.innerText);
+  copyCount.innerText = count + 1;
+});
+
+document.querySelector(".heartFour").addEventListener("click", function () {
+const heartCount = document.querySelector(".heartCount");
+     const count = parseInt(heartCount.innerText);
+     heartCount.innerText = count + 1;
+});
+
+// Five
+
+document.querySelector(".collFive").addEventListener("click", function () {
+ alert("Women & Child Helpline 109");
+
+ const collCount = document.querySelector(".collCount");
+ const count = parseInt(collCount.innerText);
+ if (count > !20) {
+   collCount.innerText = count - 20;
+ } else {
+   alert("You don't have enough coins.");
+ }
+});
+
+document.querySelector(".copyFive").addEventListener("click", function () {
+  const copyCount = document.querySelector(".copyCount");
+  const count = parseInt(copyCount.innerText);
+  copyCount.innerText = count + 1;
+});
+
+document.querySelector(".heartFive").addEventListener("click", function () {
+const heartCount = document.querySelector(".heartCount");
+     const count = parseInt(heartCount.innerText);
+     heartCount.innerText = count + 1;
+});
+
+
+// Six
+
+document.querySelector(".collSix").addEventListener("click", function () {
+ alert("Anti-Corruption Helpline 106");
+
+ const collCount = document.querySelector(".collCount");
+ const count = parseInt(collCount.innerText);
+ if (count > !20) {
+   collCount.innerText = count - 20;
+ } else {
+   alert("You don't have enough coins.");
+ }
+});
+
+document.querySelector(".copySix").addEventListener("click", function () {
+  const copyCount = document.querySelector(".copyCount");
+  const count = parseInt(copyCount.innerText);
+  copyCount.innerText = count + 1;
+});
+
+document.querySelector(".heartSix").addEventListener("click", function () {
+const heartCount = document.querySelector(".heartCount");
+     const count = parseInt(heartCount.innerText);
+     heartCount.innerText = count + 1;
+});
+
+
+
+// Seven
+
+document.querySelector(".collSeven").addEventListener("click", function () {
+  alert("Electricity Helpline 16216");
+
+  const collCount = document.querySelector(".collCount");
+  const count = parseInt(collCount.innerText);
+  if (count > !20) {
+    collCount.innerText = count - 20;
+  } else {
+    alert("You don't have enough coins.");
+  }
+});
+
+document.querySelector(".copySeven").addEventListener("click", function () {
+  const copyCount = document.querySelector(".copyCount");
+  const count = parseInt(copyCount.innerText);
+  copyCount.innerText = count + 1;
+});
+
+document.querySelector(".heartSeven").addEventListener("click", function () {
+  const heartCount = document.querySelector(".heartCount");
+  const count = parseInt(heartCount.innerText);
+  heartCount.innerText = count + 1;
+});
+
+
+// Eight
+
+document.querySelector(".collEight").addEventListener("click", function () {
+  alert("Brac Helpline 16445");
+
+  const collCount = document.querySelector(".collCount");
+  const count = parseInt(collCount.innerText);
+  if (count > !20) {
+    collCount.innerText = count - 20;
+  } else {
+    alert("You don't have enough coins.");
+  }
+});
+
+document.querySelector(".copyEight").addEventListener("click", function () {
+  const copyCount = document.querySelector(".copyCount");
+  const count = parseInt(copyCount.innerText);
+  copyCount.innerText = count + 1;
+});
+
+document.querySelector(".heartEight").addEventListener("click", function () {
+  const heartCount = document.querySelector(".heartCount");
+  const count = parseInt(heartCount.innerText);
+  heartCount.innerText = count + 1;
+});
+
+
+// Nine
+
+document.querySelector(".collNine").addEventListener("click", function () {
+  alert("Bangladesh Railway Helpline  163");
+
+  const collCount = document.querySelector(".collCount");
+  const count = parseInt(collCount.innerText);
+  if (count > !20) {
+    collCount.innerText = count - 20;
+  } else {
+    alert("You don't have enough coins.");
+  }
+});
+
+document.querySelector(".copyNine").addEventListener("click", function () {
+  const copyCount = document.querySelector(".copyCount");
+  const count = parseInt(copyCount.innerText);
+  copyCount.innerText = count + 1;
+});
+
+document.querySelector(".heartNine").addEventListener("click", function () {
+  const heartCount = document.querySelector(".heartCount");
+  const count = parseInt(heartCount.innerText);
+  heartCount.innerText = count + 1;
+});
